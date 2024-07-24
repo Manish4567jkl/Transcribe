9e070df7a0cf17006fe9b78791f91254704f06d0
Manish4567jkl <manishmandal895@gmail.com>
2024-07-24 16:41:22 +0530

    implemented recording

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -54,6 +54,7 @@ function Home({setFile,setAudioStream}) {
       })
       setAudioStream(audioBlob)
       setAudioChunks([])
+      setDuration(0)
     }
   }
   
@@ -67,27 +68,26 @@ function Home({setFile,setAudioStream}) {
   },[])
 
   return (
-    <main className='p-4 flex flex-1 flex-col justify-center gap-3 sm:gap-4 md:gap-5 text-center pb-20'>
-        <h1 className='text-5xl font-semibold sm:text-6xl md:text-7xl '>
-          Trans<span className='text-violet-400 text-5xl sm:text-6xl md:text-7xl bold'>Scribe</span>
-        </h1>
+    <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
+        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-violet-400 bold'>Scribe</span></h1>
         <h3 className='font-medium md:text-lg'>Record <span className='text-violet-400'>&rarr;</span> Transcribe <span className='text-violet-400'>&rarr;</span> Translate</h3>
-        <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-2xl'>
-          <p className='text-violet-400'>Record</p>
-          <i className="fa-solid fa-microphone"></i>
+        <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+            <p className='text-violet-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
+            <div className='flex items-center gap-2'>
+                {/* {duration !== 0 && (
+                    <p className='text-sm'>{duration}s</p>
+                )} */}
+                <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? ' text-red-500' : "")}></i>
+            </div>
         </button>
+        <p className='text-base'>Or <label className='text-violet-400 cursor-pointer hover:text-violet-600 duration-200'>upload <input onChange={(e) => {
+            const tempFile = e.target.files[0]
+            setFile(tempFile)
+        }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
         
-          <p className='text-base'>Or <label className='text-violet-400 cursor-pointer hover:text-violet-600 duration-200'>Upload <input onChange={
-            (e) => {
-              const tempFile = e.target.files[0]
-             setFile(tempFile)
-            }
-          } type="file"  className='hidden' accept='.mp3,.wave' /></label>
-          a mp3 file
-          </p>
-       
     </main>
-  )
+)
+
 } 
 
 export default Home
